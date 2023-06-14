bc60be8f8c1717f2cc6940a73be99eaf68c07dbf
Ephrem Beaino <ephrem@getray.com>
2023-06-14 09:22:51 -0700

    Initial Commit

---
+++ FormsSUI/FormsSUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  FormsSUIApp.swift
+//  FormsSUI
+//
+//  Created by Ephrem RAY on 14/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct FormsSUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
